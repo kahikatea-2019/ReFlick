2327a677598fb6291f1d77f731bc52a08db4a53d
Alice Gatland <unrelated@gmail.com>
2019-05-09 11:39:37 +1200

    Can change the colour of a pixel by clicking on it

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -2,37 +2,88 @@ import React from 'react'
 
 class Canvas extends React.Component {
   componentDidMount () {
-    this.updateCanvas()
+    this.initCanvas()
   }
 
-  updateCanvas () {
-    const context = this.refs.canvas.getContext('2d')
+  redBackground (imageData) {
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      imageData.data[i + 0] = 255
+      imageData.data[i + 1] = 0
+      imageData.data[i + 2] = 0
+      imageData.data[i + 3] = 255
+    }
 
+    return imageData
+  }
+
+  initCanvas () {
+    const context = this.refs.canvas.getContext('2d')
+    const imageData = context.createImageData(500, 500)
+
+    // const updatedImageData = this.randomPixels(imageData)
+    const updatedImageData = this.redBackground(imageData)
+
+    this.drawCanvas(updatedImageData)
+    return updatedImageData
+  }
+
+  updateCanvas (x, y) {
+    const context = this.refs.canvas.getContext('2d')
+    const imageData = context.getImageData(0, 0, 500, 500)
+    const updatedImageData = this.setPixel(imageData, x, y)
+
+    this.drawCanvas(updatedImageData)
+  }
+
+  drawCanvas (imageData) {
+    const context = this.refs.canvas.getContext('2d')
+    context.putImageData(imageData, 0, 0)
+    console.log('canvas drawn!', imageData)
+  }
+
+  setPixel (imageData, x, y, r = 0, g = 255, b = 0, a = 255) {
+    const i = (x + y * imageData.width) * 4
+    console.log('index', i)
+    imageData.data[i + 0] = r
+    imageData.data[i + 1] = g
+    imageData.data[i + 2] = b
+    imageData.data[i + 3] = a
+
+    return imageData
+  }
+
+  randomPixels (imageData) {
     function getRandomNum (min, max) {
       return Math.random() * (max - min) + min
     }
-
-    const imageData = context.createImageData(1000, 1000)
     for (let i = 0; i < imageData.data.length; i += 4) {
       imageData.data[i + 0] = getRandomNum(255, 0)
       imageData.data[i + 1] = getRandomNum(255, 0)
       imageData.data[i + 2] = getRandomNum(255, 0)
       imageData.data[i + 3] = getRandomNum(255, 0)
     }
-
-    context.putImageData(imageData, 0, 0)
+    return imageData
   }
 
   clickHandler = e => {
-    this.updateCanvas()
+    this.initCanvas()
+  }
+
+  pixelClickHandler = e => {
+    const { offsetX: x, offsetY: y } = e.nativeEvent
+    this.updateCanvas(x, y)
   }
 
   render () {
     return (
       <div>
-        <canvas ref="canvas" width={300} height={300}/>
+        <canvas
+          ref="canvas"
+          width={500}
+          height={500}
+          onClick={this.pixelClickHandler}/>
         <button
-          onClick={() => this.clickHandler()}>
+          onClick={this.clickHandler}>
           Clickme
         </button>
       </div>
