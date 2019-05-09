be856ab99ec7c3eb27f43965cf56cc271cf7d194
Alice Gatland <unrelated@gmail.com>
2019-05-09 14:32:52 +1200

    Can paint with a square now

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -2,77 +2,99 @@ import React from 'react'
 
 class Canvas extends React.Component {
   state = {
-    mouseDown: false
+    mouseDown: false,
+    context: null
   }
+
   componentDidMount () {
     this.initCanvas()
   }
 
-  redBackground (imageData) {
-    for (let i = 0; i < imageData.data.length; i += 4) {
-      imageData.data[i + 0] = 255
-      imageData.data[i + 1] = 0
-      imageData.data[i + 2] = 0
-      imageData.data[i + 3] = 255
-    }
-
-    return imageData
-  }
-
-  initCanvas () {
+  initCanvas = () => {
     const context = this.refs.canvas.getContext('2d')
+    this.setState({
+      context
+    })
     const imageData = context.createImageData(500, 500)
 
-    // const updatedImageData = this.randomPixels(imageData)
-    const updatedImageData = this.redBackground(imageData)
+    // const updatedImageData = this.setBackgroundRandom(imageData)
+    const updatedImageData = this.setBackground(imageData)
 
-    this.drawCanvas(updatedImageData)
-    return updatedImageData
+    context.putImageData(updatedImageData, 0, 0)
   }
 
-  updateCanvas (x, y) {
-    const context = this.refs.canvas.getContext('2d')
-    const imageData = context.getImageData(0, 0, 500, 500)
-    const updatedImageData = this.setPixel(imageData, x, y)
-
-    this.drawCanvas(updatedImageData)
+  updateCanvas = (x, y) => {
+    const imageData = this.state.context.getImageData(0, 0, 500, 500)
+    const updatedImageData = this.paintPixels(imageData, x, y, 10)
+    this.state.context.putImageData(updatedImageData, 0, 0)
   }
 
-  drawCanvas (imageData) {
-    const context = this.refs.canvas.getContext('2d')
-    context.putImageData(imageData, 0, 0)
+  // Sets background colour, default is black
+  setBackground = (imageData, r = 0, g = 0, b = 0, a = 255) => {
+    const { data } = imageData
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      data[i + 0] = r
+      data[i + 1] = g
+      data[i + 2] = b
+      data[i + 3] = a
+    }
+    return imageData
   }
 
-  setPixel (imageData, x, y, r = 0, g = 255, b = 0, a = 255) {
-    const i = (x + y * imageData.width) * 4
-    imageData.data[i + 0] = r
-    imageData.data[i + 1] = g
-    imageData.data[i + 2] = b
-    imageData.data[i + 3] = a
+  // Changes colour for a square of size x size pixels
+  paintPixels (imageData, x, y, size, r = 0, g = 255, b = 0, a = 255) {
+    const { width, data } = imageData
+    const centreIndex = (x + y * width) * 4
+    const topLeft = centreIndex - (size / 2 * 4)
+
+    const pixelsToPaint = []
+    for (let i = 0; i < size; i++) {
+      const top = (topLeft + 4 * i)
+      for (let j = 0; j < size; j++) {
+        pixelsToPaint.push(top - (width * 4 * j))
+      }
+    }
+
+    pixelsToPaint.forEach(i => {
+      data[i + 0] = r
+      data[i + 1] = g
+      data[i + 2] = b
+      data[i + 3] = a
+    })
 
     return imageData
   }
 
-  randomPixels (imageData) {
+  // Sets background to random pixel colours
+  setBackgroundRandom = (imageData) => {
     function getRandomNum (min, max) {
       return Math.random() * (max - min) + min
     }
+    const { data } = imageData
     for (let i = 0; i < imageData.data.length; i += 4) {
-      imageData.data[i + 0] = getRandomNum(255, 0)
-      imageData.data[i + 1] = getRandomNum(255, 0)
-      imageData.data[i + 2] = getRandomNum(255, 0)
-      imageData.data[i + 3] = getRandomNum(255, 0)
+      data[i + 0] = getRandomNum(255, 0)
+      data[i + 1] = getRandomNum(255, 0)
+      data[i + 2] = getRandomNum(255, 0)
+      data[i + 3] = getRandomNum(255, 0)
     }
     return imageData
   }
 
-  clickHandler = e => {
+  reset = e => {
     this.initCanvas()
   }
 
   mouseDownHandler = e => {
     this.setState({
       mouseDown: true
+    })
+    const { offsetX: x, offsetY: y } = e.nativeEvent
+    this.updateCanvas(x, y)
+  }
+
+  mouseUpHandler = e => {
+    this.setState({
+      mouseDown: false
     })
   }
 
@@ -81,12 +103,6 @@ class Canvas extends React.Component {
       const { offsetX: x, offsetY: y } = e.nativeEvent
       this.updateCanvas(x, y)
     }
-  }
-
-  mouseUpHandler = e => {
-    this.setState({
-      mouseDown: false
-    })
   }
 
   render () {
@@ -100,8 +116,8 @@ class Canvas extends React.Component {
           onMouseMove={this.mouseMoveHandler}
           onMouseUp={this.mouseUpHandler}/>
         <button
-          onClick={this.clickHandler}>
-          Clickme
+          onClick={this.reset}>
+            Reset
         </button>
       </div>
     )
