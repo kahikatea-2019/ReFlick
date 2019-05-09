08abea1fe9478f2acc77a470b186c78908875106
MurdocAV <azaan.virk1@gmail.com>
2019-05-10 11:30:46 +1200

    Got rid of comments

--- client/components/Colour.jsx
+++ client/components/Colour.jsx
@@ -31,25 +31,6 @@ export default class Colour extends React.Component {
 }
 
 // TODO: Need to be able to set an active frame ->
-// 1. Refactor component into a class to be able to store via state
-// 2. Check whether a frame is set as active
-// 2a. Default value should be no frame (null)
-
-// export default function Colour (props) {
-//   return (
-//     <React.Fragment>
-//       <Dropdown>
-//         <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-//           <div className='colour' style={{width: 30, height: 30, backgroundColor: props.colour}}>
-//             <Dropdown.Menu as={CustomMenu}>
-//               <ul>
-//                 {console.log(returnDropdowns(props.colour, props.frames))}
-//               </ul>
-//             </Dropdown.Menu>
-//           </div>
-//         </Dropdown.Toggle>
-//       </Dropdown>
-//       <br/>
-//     </React.Fragment>
-//   )
-// }
+// 1. [X] Refactor component into a class to be able to store via state
+// 2. [ ] Check whether a frame is set as active
+// 2a. [ ] Default value should be no frame (null)
