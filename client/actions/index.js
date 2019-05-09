7411ee231cef69428ddb62d5e42857e9c045d48d
Alice Gatland <unrelated@gmail.com>
2019-05-09 16:11:01 +1200

    Setup action and reducer for brush colour

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,5 +1,6 @@
 export const GET_COLOURS = 'GET_COLOURS'
 export const GET_FRAMES = 'GET_FRAMES'
+export const SET_BRUSH_COLOUR = 'SET_BRUSH_COLOUR'
 
 export const getColours = (colours) => {
   return {
@@ -11,5 +12,12 @@ export const getFrames = (colours) => {
   return {
     type: GET_FRAMES,
     frames
+  }
+}
+
+export const setBrushColour = (colour) => {
+  return {
+    type: SET_BRUSH_COLOUR,
+    colour
   }
 }
