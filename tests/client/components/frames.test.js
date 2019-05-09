43060de534a053b709484217f4cb0050c61b4328
John Winiata Tapsell <johntapsell@windowslive.com>
2019-05-10 11:01:08 +1200

    testing header , footer and frame done

---
+++ tests/client/components/frames.test.js
@@ -0,0 +1,13 @@
+// import Frames from '../../../client/components/Frames'
+// import { shallow, mount } from 'enzyme'
+// import React from 'react'
+
+// test('test runner is working', () => {
+//   expect(true).toBeTruthy()
+// })
+// let wrapper = shallow(<Frames />)
+// describe('<Frames is running', () => {
+//   it('Renders correct', () => {
+//     expect(wrapper).toMatchSnapshot()
+//   })
+// })
