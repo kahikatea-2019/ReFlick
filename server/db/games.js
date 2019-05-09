29a8b6f5a631c42c35322fcbb62fe0cf77040847
Struan Wall <struan03@hotmail.com>
2019-05-09 14:38:34 +1200

    make db server functions

---
+++ server/db/games.js
@@ -0,0 +1,23 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+module.export = {
+  getGame,
+  submitGame
+}
+
+// retrieve saved game so that user can play
+
+function getGame (id, db = connection) {
+  return db('games')
+    .where({ id: id })
+    .select()
+}
+
+// export button to submit to db once user has built game
+
+function submitGame (frame1Img, frame1Map, frame2Img, frame2Map, frame3Img, frame3Map, frame4Img, frame4Map, db = connection) {
+  return db('games')
+    .insert({ frame1Img, frame1Map, frame2Img, frame2Map, frame3Img, frame3Map, frame4Img, frame4Map })
+}
