bf5174add1ce5274d2ac0119bab8f0d3ca2f7c60
John Winiata Tapsell <johntapsell@windowslive.com>
2019-05-11 12:46:02 +1200

    set dispatches for pen size

--- client/components/Pen.jsx
+++ client/components/Pen.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import Button from 'react-bootstrap'
+import { connect } from 'react-redux'
 
-export default function Pen () {
-  const smallPen = () => {
-    alert('small pen')
-  }
-  const medPen = () => {
-    alert('med pen')
-  }
-  const largePen = () => {
-    alert('large pen')
-  }
+import { setBrushSize } from '../actions/index'
 
+function Pen (props) {
   return (
     <>
       <h3>Pens</h3>
-      <button className='sbutton' onClick={smallPen}></button>
-      <button onClick={medPen}></button>
-      <button className='lbutton' onClick={largePen}></button>
+      <button className='sbutton' onClick={props.smallPen}></button>
+      <button className='mbutton'onClick={props.medPen}></button>
+      <button className='lbutton' onClick={props.largePen}></button>
     </>
   )
 }
+
+function mapStateToDispatch (dispatch) {
+  return {
+    smallPen: () => dispatch(setBrushSize(10)),
+    medPen: () => dispatch(setBrushSize(20)),
+    largePen: () => dispatch(setBrushSize(30))
+  }
+}
+
+export default connect(null, mapStateToDispatch)(Pen)
