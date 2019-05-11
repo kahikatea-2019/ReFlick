b1b626bd9bc07841c2d3b6a3bf8b4a260ceb4561
MurdocAV <azaan.virk1@gmail.com>
2019-05-11 13:07:58 +1200

    Using jest-enzyme for testing

--- tests/client/components/canvas.test.js
+++ tests/client/components/canvas.test.js
@@ -3,6 +3,10 @@ import Canvas from '../../../client/components/canvas/Canvas'
 import * as enzyme from 'enzyme'
 import * as Adapter from 'enzyme-adapter-react-16'
 
+test('test harness is setup correctly', () => {
+  expect(true).toBe(true)
+})
+
 Canvas.prototype.componentDidMount = () => {}
 test('Test runner is working', () => {
   expect(true).toBeTruthy()
