5c72a36949ecf735be539b9ca3ae73454c242b38
Eve Te Uru Henare <evehenare1@gmail.com>
2019-05-13 11:28:23 +1200

    Fixed  path directories and script for knexfile move to db folder

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -10,10 +10,10 @@ module.exports = {
     },
     useNullAsDefault: true,
     migrations: {
-      directory: path.join(__dirname, 'server/db/migrations')
+      directory: path.join(__dirname, 'migrations')
     },
     seeds: {
-      directory: path.join(__dirname, 'server/db/seeds')
+      directory: path.join(__dirname, 'seeds')
     }
   },
 
@@ -26,10 +26,10 @@ module.exports = {
   },
 
   // migrations: {
-  //   directory: path.join(__dirname, 'server/db/migrations')
+  //   directory: path.join(__dirname, 'migrations')
   // },
   // seeds: {
-  //   directory: path.join(__dirname, 'server/db/seeds')
+  //   directory: path.join(__dirname, 'seeds')
   // }
 
   staging: {
