4be35060e5d41c631527ac1817b3c71f229c44cc
Alice Gatland <unrelated@gmail.com>
2019-05-13 11:12:05 +1200

    Playable games!

--- client/components/canvas/PlayCanvas.jsx
+++ client/components/canvas/PlayCanvas.jsx
@@ -1,21 +1,16 @@
 import React from 'react'
-import { getGameData } from '../../api/games'
 import { connect } from 'react-redux'
+
+import { getGameData } from '../../api/games'
 
 class PlayCanvas extends React.Component {
   state = {
     currentFrame: 1,
-    // colours: {
-    //   red: [191, 63, 63, 255],
-    //   blue: [63, 127, 191, 255],
-    //   green: [63, 191, 63, 255],
-    //   pink: [191, 63, 127, 255]
-    // }
     colours: this.props.colours
   }
 
   componentDidMount () {
-    getGameData(23)
+    getGameData(24)
       .then(game => {
         const frameIndices = [1, 2, 3, 4]
         frameIndices.forEach(i => {
@@ -31,28 +26,44 @@ class PlayCanvas extends React.Component {
       })
   }
 
-  findPixelColour (e) {
+  displayActiveFrame = () => {
+    const context = this.refs.playcanvas.getContext('2d')
+    const frameImg = this.state[`frame${this.state.currentFrame}Img`]
+    context.putImageData(frameImg, 0, 0)
+  }
+
+  clickHandler = (e) => {
     const { offsetX, offsetY } = e.nativeEvent
     const context = this.refs.playcanvas.getContext('2d')
-    const pixelColour = Array.from(context.getImageData(offsetX, offsetY, 1, 1).data)
-    console.log(this.state)
-    console.log(this.state.game.frame1Map)
+    const pixelClicked = Array.from(context.getImageData(offsetX, offsetY, 1, 1).data)
 
-    if (JSON.stringify(pixelColour) === JSON.stringify(this.state.colours.red)) {
-      this.setState({
-        currentFrame: this.state.game.frame1Map
+    const coloursArray = this.state.colours.map(colourObj => {
+      const { r, g, b, a } = colourObj
+      return [ r, g, b, a ]
+    })
+
+    const checkPixelColour = (pixelClicked) => {
+      coloursArray.forEach((colour, i) => {
+        if (JSON.stringify(pixelClicked) === JSON.stringify(colour)) {
+          const colourId = this.state.colours[i].id
+          const frameMap = this.state[`frame${this.state.currentFrame}Map`]
+          const frameToSwitchTo = frameMap[`col${colourId}`]
+          if (frameToSwitchTo) {
+            this.setState({
+              currentFrame: frameToSwitchTo
+            }, this.displayActiveFrame)
+          }
+        }
       })
-    // } else if (JSON.stringify(pixelColour) === JSON.stringify(this.state.colours.blue)) {
-    //   this.setState({
-    //     currentFrame: this.state.game.frame1Map.col2
-    //   })
     }
+    checkPixelColour(pixelClicked)
   }
+
   render () {
     return (
       <div id ="playcanvas">
         <canvas
-          onClick={this.findPixelColour.bind(this)}
+          onClick={this.clickHandler}
           ref="playcanvas"
           width={500}
           height={500}>
@@ -64,11 +75,8 @@ class PlayCanvas extends React.Component {
 
 function mapStateToProps (state) {
   return ({
-    colours: state.coloursReducer
+    colours: state.coloursReducer.colours
   })
 }
 
 export default connect(mapStateToProps)(PlayCanvas)
-
-// Find pixel colour
-// If pixel colour === colour number { change frame to colour assigned frame}
