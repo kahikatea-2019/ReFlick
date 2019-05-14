8df17adfb648e30b495693d57e2f4a53789f53b9
Eve Te Uru Henare <evehenare1@gmail.com>
2019-05-14 15:16:07 +1200

    api test passing

--- tests/client/api/games.api.test.js
+++ tests/client/api/games.api.test.js
@@ -1,13 +1,13 @@
 import nock from 'nock'
 
-import { getGamesData } from '../../../client/api/game'
+import { getGameData } from '../../../client/api/games'
 
-test('getGamesData returns game data', () => {
+test('getGameData returns game data', () => {
   const scope = nock('http://localhost:3000')
-    .get('/api/game')
+    .get(`/api/v1/games/1`)
     .reply(200)
 
-  getGamesData((err) => {
+  getGameData((err) => {
     expect(err).toBeFalsy()
     scope.done()
   })
